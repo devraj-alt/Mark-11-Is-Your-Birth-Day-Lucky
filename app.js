b2ec6ff2c82089d6f55573d563104f7643586b07
devraj-alt <mr.devrajroy@outlook.com>
2022-10-19 16:28:00 +0530

    fix: minor fix

--- app.js
+++ app.js
@@ -6,21 +6,29 @@ var showResult = document.querySelector("#show-result");
 
 function checkLuckyNumber() {
     var addDate = birthDate.value
-    var dateArray = addDate.split("-") //replaceAll = split
-
-    var sumAll = 0
-
-    for (let i = 0; i < dateArray.length; i++) {
-        var yrMmDd = Number(dateArray[i]) //type casting
-        sumAll = sumAll + yrMmDd
+    var dateArray = addDate.split("-")
+    if(!addDate){
+        showResult.innerText = "Invalid date"
+        return
     }
-
+    var sumAll = 0
     var luckyNum = Number(luckyNumber.value)
 
-    if (sumAll % luckyNum === 0) {
-        showResult.innerText = "Your Birthday is Lucky"
-    } else {
-        showResult.innerText = "Sorry your Birthday is not Lucky"
+    if(luckyNum > 0){
+        
+        for (let i = 0; i < dateArray.length; i++) {
+            var yrMmDd = Number(dateArray[i])
+            sumAll = sumAll + yrMmDd
+        }
+        
+        if (sumAll % luckyNum === 0) {
+            showResult.innerText = "Your Birthday is Lucky"
+        } else {
+            showResult.innerText = "Sorry your Birthday is not Lucky"
+        }
+        
+    }else{
+        showResult.innerText = "Invalid lucky number"
     }
 }
 
